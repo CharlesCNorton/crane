94577ae55d57b80c04a97855ed5f6c770f0a1efc
CharlesCNorton <machineelv@gmail.com>
2026-02-23 09:20:52 -0500

    Add wip test for Function vernacular extraction (#31)

---
+++ tests/wip/function_vernac/func_vernac.h
@@ -0,0 +1,107 @@
+#include <algorithm>
+#include <any>
+#include <cassert>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <optional>
+#include <stdexcept>
+#include <string>
+#include <utility>
+#include <variant>
+
+template <typename F, typename R, typename... Args>
+concept MapsTo = std::is_invocable_r_v<R, F &, Args &...>;
+
+template <class... Ts> struct Overloaded : Ts... {
+  using Ts::operator()...;
+};
+template <class... Ts> Overloaded(Ts...) -> Overloaded<Ts...>;
+
+struct List {
+  template <typename A> struct list {
+  public:
+    struct nil {};
+    struct cons {
+      A _a0;
+      std::shared_ptr<List::list<A>> _a1;
+    };
+    using variant_t = std::variant<nil, cons>;
+
+  private:
+    variant_t v_;
+    explicit list(nil _v) : v_(std::move(_v)) {}
+    explicit list(cons _v) : v_(std::move(_v)) {}
+
+  public:
+    struct ctor {
+      ctor() = delete;
+      static std::shared_ptr<List::list<A>> nil_() {
+        return std::shared_ptr<List::list<A>>(new List::list<A>(nil{}));
+      }
+      static std::shared_ptr<List::list<A>>
+      cons_(A a0, const std::shared_ptr<List::list<A>> &a1) {
+        return std::shared_ptr<List::list<A>>(new List::list<A>(cons{a0, a1}));
+      }
+      static std::unique_ptr<List::list<A>> nil_uptr() {
+        return std::unique_ptr<List::list<A>>(new List::list<A>(nil{}));
+      }
+      static std::unique_ptr<List::list<A>>
+      cons_uptr(A a0, const std::shared_ptr<List::list<A>> &a1) {
+        return std::unique_ptr<List::list<A>>(new List::list<A>(cons{a0, a1}));
+      }
+    };
+    const variant_t &v() const { return v_; }
+    variant_t &v_mut() { return v_; }
+  };
+};
+
+struct Sig0 {
+  template <typename A> struct sig0 {
+  public:
+    struct exist {
+      A _a0;
+    };
+    using variant_t = std::variant<exist>;
+
+  private:
+    variant_t v_;
+    explicit sig0(exist _v) : v_(std::move(_v)) {}
+
+  public:
+    struct ctor {
+      ctor() = delete;
+      static std::shared_ptr<Sig0::sig0<A>> exist_(A a0) {
+        return std::shared_ptr<Sig0::sig0<A>>(new Sig0::sig0<A>(exist{a0}));
+      }
+      static std::unique_ptr<Sig0::sig0<A>> exist_uptr(A a0) {
+        return std::unique_ptr<Sig0::sig0<A>>(new Sig0::sig0<A>(exist{a0}));
+      }
+    };
+    const variant_t &v() const { return v_; }
+    variant_t &v_mut() { return v_; }
+  };
+};
+
+std::shared_ptr<Sig0::sig0<unsigned int>> div2_terminate(const unsigned int n);
+
+unsigned int div2(const unsigned int);
+
+std::shared_ptr<Sig0::sig0<unsigned int>>
+list_sum_terminate(const std::shared_ptr<List::list<unsigned int>> &l);
+
+unsigned int list_sum(const std::shared_ptr<List::list<unsigned int>> &);
+
+const unsigned int test_div2 =
+    div2(((((((((((0 + 1) + 1) + 1) + 1) + 1) + 1) + 1) + 1) + 1) + 1));
+
+const unsigned int test_sum = list_sum(List::list<unsigned int>::ctor::cons_(
+    (0 + 1),
+    List::list<unsigned int>::ctor::cons_(
+        ((0 + 1) + 1), List::list<unsigned int>::ctor::cons_(
+                           (((0 + 1) + 1) + 1),
+                           List::list<unsigned int>::ctor::cons_(
+                               ((((0 + 1) + 1) + 1) + 1),
+                               List::list<unsigned int>::ctor::cons_(
+                                   (((((0 + 1) + 1) + 1) + 1) + 1),
+                                   List::list<unsigned int>::ctor::nil_()))))));
